593d02dd6247fc3a9481b820b7321eb821f52044
BekaChkhiro <bekachkhirodze1@gmail.com>
2023-03-28 22:48:28 +0400

    Finished

--- js/app.js
+++ js/app.js
@@ -7,9 +7,21 @@ items.forEach(item => {
     const title = item.querySelector('.header-title');
 
     trigger.addEventListener('click', () => {
+        items.forEach(item => {
+            const trigger = item.querySelector('.accordion-header');
+            const body = item.querySelector('.accordion-description');
+            const accImg = item.querySelector('#arrow');
+            const title = item.querySelector('.header-title');
+
+            item.querySelector('.accordion-header').classList.remove('open');
+            title.classList.remove('bold');
+            body.classList.remove('active');
+            accImg.classList.remove('acc-img');
+        })
         item.querySelector('.accordion-header').classList.toggle('open');
         title.classList.toggle('bold');
         body.classList.toggle('active');
         accImg.classList.toggle('acc-img');
     });
+    
 });
